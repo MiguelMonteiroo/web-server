e0e3c2d7ad8cd9959f57c448b30e1a76e2d5fee5
miguel <monteiro.miguel05@gmail.com>
2025-12-09 03:42:40 -0300

    refact: convert to promise-based style

--- tcp.ts
+++ tcp.ts
@@ -1,24 +1,103 @@
 import * as net from 'net';
+type TCPConn = {
+    socket: net.Socket;
+    err: null | Error;
+    ended: boolean
+    reader: null | {
+        resolve: (value: Buffer) => void,
+        reject: (reason: Error) => void,
+    };
+};
 
+function soInit(socket: net.Socket): TCPConn {
+    const conn: TCPConn = {
+        socket: socket, err: null, ended: false, reader: null,
+    };
+    socket.on('data', (data: Buffer) => {
+        console.assert(conn.reader);
+        conn.socket.pause();
+        conn.reader!.resolve(data);
+        conn.reader = null;
 
-function newConn(socket: net.Socket): void{
-    console.log('new connection', socket.remoteAddress, socket.remotePort)  
-    socket.on('end', () => {
-        console.log('EOF.')
     });
-    socket.on('data', (data: Buffer) =>{
-        console.log('data:', data)
-        socket.write(data);
-        
-        if(data.includes('q')){
-            console.log('closing');
-            socket.end();
+
+    socket.on('end', () => {
+        conn.ended = true;
+        if (conn.reader) {
+            conn.reader.resolve(Buffer.from(''));
+            conn.reader = null;
+        }
+    });
+    socket.on('error', (err: Error) => {
+        conn.err = err;
+        if (conn.reader) {
+            conn.reader.reject(err);
+            conn.reader = null;
         }
     })
 
+    return conn;
 }
 
-let server = net.createServer();
+function soRead(conn: TCPConn): Promise<Buffer> {
+    console.assert(!conn.reader);
+    return new Promise((resolve, reject) => {
+        if(conn.err){
+            reject(conn.err);
+            return;
+        }
+        if(conn.ended){
+            resolve(Buffer.from(''));
+            return;
+        }
+        conn.reader = { resolve: resolve, reject: reject };
+        conn.socket.resume();
+    })
+}
+
+function soWrite(conn: TCPConn, data: Buffer): Promise<void> {
+    console.assert(data.length > 0);
+    return new Promise((resolve, reject) => {
+        if (conn.err) {
+            reject(conn.err);
+            return;
+        }
+        conn.socket.write(data, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+async function serveClient(socket: net.Socket) {
+    const conn: TCPConn = soInit(socket);
+    while(true){
+        const data = await soRead(conn);
+        if(data.length === 0){
+            console.log('end connection');
+            break;
+        }
+        console.log('data:', data);
+        await soWrite(conn, data);
+    }    
+}
+
+async function newConn(socket: net.Socket): Promise<void> {
+    console.log('new connection', socket.remoteAddress, socket.remotePort);
+    try{
+        await serveClient(socket);
+    } catch(exc){
+        console.log('exception:', exc);
+    } finally{
+        socket.destroy();
+    }
+
+}
+
+let server = net.createServer({ pauseOnConnect: true });
 server.on('connection', newConn);
-server.on('error', (err: Error) => {throw err;});
-server.listen({host:'127.0.0.1', port: 1234});
+server.on('error', (err: Error) => { throw err; });
+server.listen({ host: '127.0.0.1', port: 1234 });
