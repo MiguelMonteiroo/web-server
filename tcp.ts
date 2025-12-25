79dd89ca5d2c7e923c3c4bfd7beb0c417a3ad04f
miguel <monteiro.miguel05@gmail.com>
2025-12-25 07:26:54 -0300

    simple protocol with dynamic buffer

--- tcp.ts
+++ tcp.ts
@@ -1,3 +1,4 @@
+import { Socket } from 'dgram';
 import * as net from 'net';
 
 type TCPConn = {
@@ -18,6 +19,28 @@ type TCPListener = {
         reject: (reason: Error) => void,
     };
 };
+
+
+type DynBuf = {
+    data: Buffer,
+    length: number
+};
+
+function bufPush(buf: DynBuf, data: Buffer): void {
+    const newLen = buf.length + data.length;
+    if (buf.data.length < newLen) {
+        let cap = Math.max(buf.data.length, 32);
+
+        while (cap < newLen) {
+            cap *= 2;
+        }
+        const grown = Buffer.alloc(cap);
+        buf.data.copy(grown, 0, 0)
+        buf.data = grown;
+    }
+    data.copy(buf.data, buf.length, 0);
+    buf.length = newLen;
+}
 
 
 
@@ -54,11 +77,11 @@ function soInit(socket: net.Socket): TCPConn {
 function soRead(conn: TCPConn): Promise<Buffer> {
     console.assert(!conn.reader);
     return new Promise((resolve, reject) => {
-        if(conn.err){
+        if (conn.err) {
             reject(conn.err);
             return;
         }
-        if(conn.ended){
+        if (conn.ended) {
             resolve(Buffer.from(''));
             return;
         }
@@ -84,26 +107,58 @@ function soWrite(conn: TCPConn, data: Buffer): Promise<void> {
     });
 }
 
-async function serveClient(conn: TCPConn): Promise<void> {
-    while(true){
-        const data = await soRead(conn);
-        if(data.length === 0){
-            console.log('end connection');
-            break;
-        }
-        console.log('data:', data);
-        await soWrite(conn, data);
-    }    
+
+
+function cutMessage(buf:DynBuf): null|Buffer{
+    const idx = buf.data.subarray(0, buf.length).indexOf('\n');
+    if(idx <0){
+        return null;
+    }
+    const msg = Buffer.from(buf.data.subarray(0, idx + 1));
+    bufPop(buf, idx + 1);
+    return  msg
 }
 
- function soListen(host: string, port: number): TCPListener {
+function bufPop(buf:DynBuf, len: number): void{
+    buf.data.copyWithin(0, len, buf.length);
+    buf.length -= len;
+}
+
+async function serveClient(conn: TCPConn): Promise<void> {
+    const buf: DynBuf = { data: Buffer.alloc(0), length: 0 };
+    while (true) {
+        const msg:null|Buffer = cutMessage(buf);
+        if (!msg) {
+            const data = await soRead(conn);
+            bufPush(buf, data);
+            
+            if (data.length === 0) {
+                console.log('end connection');
+                return;
+            }
+            continue;
+        }
+
+        if(msg.equals(Buffer.from('quit\n'))){
+            await soWrite(conn, Buffer.from('Bye.\n'));
+            conn.socket.destroy();
+            return;
+        }else{
+            const reply = Buffer.concat([Buffer.from('Echo: '), msg])
+            await soWrite(conn, reply);
+        }
+    }
+}
+
+
+function soListen(host: string, port: number): TCPListener {
     let server = net.createServer({ pauseOnConnect: true });
 
     const listener: TCPListener = {
         server,
         err: null,
         accepter: null
-    } 
+    }
 
     server.on('connection', (socket: net.Socket) => {
         console.assert(listener.accepter);
@@ -114,30 +169,30 @@ async function serveClient(conn: TCPConn): Promise<void> {
 
     server.on('error', (err: Error) => {
         listener.err = err;
-        if(listener.accepter){
+        if (listener.accepter) {
             listener.accepter.reject(err);
             listener.accepter = null;
         }
     })
-    server.listen({host, port})
+    server.listen({ host, port })
     return listener
-    
+
 }
 
-function soAccept(listener: TCPListener): Promise<TCPConn>{
+function soAccept(listener: TCPListener): Promise<TCPConn> {
     console.assert(!listener.accepter);
-    return new Promise((resolve, reject) =>{
-        if(listener.err){
+    return new Promise((resolve, reject) => {
+        if (listener.err) {
             reject(listener.err);
             return;
         }
-        listener.accepter = {resolve, reject};
+        listener.accepter = { resolve, reject };
     })
 }
 
-const listener = soListen('127.0.0.1',  1234);
+const listener = soListen('127.0.0.1', 1234);
 
-while(true){
+while (true) {
     const conn = await soAccept(listener);
     serveClient(conn).catch(err => {
         console.error('client error:', err)
